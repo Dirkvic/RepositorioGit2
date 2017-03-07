5e690ae9417473af32996d899d206e28b63b3871
Victor <Victor@192.168.1.33>
2017-03-07 10:52:37 +0100

    Clase Hola Mundo creada.

---
+++ ProyectoGit/src/paquete/HolaMundo.java
@@ -0,0 +1,15 @@
+package paquete;
+
+public class HolaMundo {
+
+	public HolaMundo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+
+	}
+
+}
